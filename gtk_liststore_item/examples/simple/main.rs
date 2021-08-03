363ed9816582338d29b132314dfb0d96d2f2a1b5
Romain Porte <microjoe@microjoe.org>
2021-08-03 17:37:16 +0200

    Add simple example

---
+++ gtk_liststore_item/examples/simple/main.rs
@@ -0,0 +1,51 @@
+use gio::prelude::*;
+use gtk::prelude::*;
+use gtk_liststore_item::ListStoreItem;
+
+use gladis::Gladis;
+
+#[derive(Gladis, Clone)]
+struct Ui {
+    window: gtk::Window,
+    list_store: gtk::ListStore,
+    tree_view: gtk::TreeView,
+}
+
+#[derive(ListStoreItem)]
+struct Item {
+    name: String,
+    value: u32,
+    progress: u32,
+    is_cool: bool,
+}
+
+fn build_ui(app: &gtk::Application) {
+    let glade_src = include_str!("simple.ui");
+    let mut ui = Ui::from_string(glade_src).unwrap();
+
+    for i in 0..10 {
+        let item = Item {
+            name: format!("foobar{}", i),
+            value: rand::random(),
+            progress: rand::random::<u32>() % 100,
+            is_cool: rand::random()
+        };
+        item.insert_into_liststore(&mut ui.list_store);
+    }
+
+    ui.window.set_application(Some(app));
+    ui.window.show_all();
+}
+
+fn main() {
+    let application = gtk::Application::new(
+        Some("com.github.MicroJoe.gtk_liststore_item.examples.simple"),
+        Default::default(),
+    );
+
+    application.connect_activate(|app| {
+        build_ui(app);
+    });
+
+    application.run();
+}
